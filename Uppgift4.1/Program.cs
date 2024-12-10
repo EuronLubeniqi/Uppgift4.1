04eb4a114bda3e0d44aef993ce99744492f343c1
EuronLubeniqi <eulu07001@utb.vaxjo.se>
2024-12-10 13:58:18 +0100

    Add project files.

---
+++ Uppgift4.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Uppgift4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 10;i<31;i++)
+                Console.WriteLine(i);
+            {
+               
+                for (int t=200;t>179;t--)
+                    Console.WriteLine(t);
+                {
+                   for (int j=100;j<1401;j=j+50)
+                        Console.WriteLine(j);
+                       
+
+                }
+
+            }
+        }
+            
+
+        
+        
+            
+
+    }
+}
+    
